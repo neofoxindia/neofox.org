7cfdca60ded430ea66530af97d211ff8af56944e
ashvinijangid <ashvinijangid@gmail.com>
2021-04-24 07:32:54 +0000

    Update from https://github.com/its-ash/neofox/commit/a005463bf939b165edad5a668768a248fe2fcaa1

---
+++ _nuxt/static/1619249569/web-application-development/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1619249569",layout:"default",error:a,serverRendered:true,routePath:"\u002Fweb-application-development",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
